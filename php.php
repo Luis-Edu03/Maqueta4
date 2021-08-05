a21cea2b22fa8db50a67065d2ea77ea5b661732a
Luis-Edu03 <luis_8902@hotmail.comgit config --global user.name Luis-Edu03git config --global user.email luis_8902@hotmail.com>
2021-08-05 00:54:10 -0500

    primer respaldo

---
+++ php.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$correo = $_POST["correo"];
+echo "Bienvenido: <br>";
+echo "Nombre: " . $nombre . "<br>";
+echo "Apellido: " . $apellido . "<br>";
+echo "Correo: " . $correo . "<br>";
+
+?>
